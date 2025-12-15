cd60b8e9c50648223a66d3b0ce610136c96646ac
va1996 <76706051+va1996@users.noreply.github.com>
2025-12-15 21:48:25 +0100

    Initial commit

---
+++ R/analysis.R
@@ -0,0 +1,31 @@
+library(here)
+
+data <- read.csv(here("Input", "data.csv"))
+
+summary(data)
+
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Torgersen")$bill_length_mm), 2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Biscoe")$bill_length_mm), 2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Dream")$bill_length_mm), 2))
+
+
+# Plot
+penguins_clean <- na.omit(data)
+plot(penguins_clean$bill_length_mm, penguins_clean$bill_depth_mm, type = "n", xlab = "Bill Length (mm)", ylab = "Bill Depth (mm)", main = "Penguin Bill Dimensions")
+points(
+  penguins_clean$bill_length_mm[penguins_clean$species == "Adelie"], penguins_clean$bill_depth_mm[penguins_clean$species == "Adelie"],
+  col = "red", pch = 16
+)
+points(penguins_clean$bill_length_mm[penguins_clean$species == "Chinstrap"], penguins_clean$bill_depth_mm[penguins_clean$species == "Chinstrap"], col = "green", pch = 17)
+points(penguins_clean$bill_length_mm[penguins_clean$species == "Gentoo"],
+  penguins_clean$bill_depth_mm[penguins_clean$species == "Gentoo"],
+  col = "blue", pch = 18
+)
+legend("topright",
+  legend = unique(penguins_clean$species),
+  col = c(
+    "red",
+    "green",
+    "blue"
+  ), pch = c(16, 17, 18)
+)
